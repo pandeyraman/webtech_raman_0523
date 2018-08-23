1a1053ef474c551938e70bc0e234b102c538cc62
pandeyraman <ramanpandey19@gmail.com>
2018-08-23 12:48:56 +0545

    changes

---
+++ include/header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ramanpandey
+ * Date: 7/11/18
+ * Time: 11:28 PM
+ */
+?>
+<html>
+<head>
+    <title>Toursim Site</title>
+    <link type="text/css" href="../assets/css/bootstrap.css" rel="stylesheet">
+    <link type="text/css" href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <nav class="site-header sticky-top py-1">
+        <div class="container d-flex flex-column flex-md-row justify-content-between">
+            <a class="py-2" href="#">
+                Home
+            </a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
+            <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
+        </div>
+    </nav>
